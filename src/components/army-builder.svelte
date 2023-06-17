93b196ada21a44f8754145ba8b80ff0c4a397248
Simon <simon@aevea.ee>
2023-06-17 23:36:39 +0200

    chore: consolidate reused code into ArmyBuilder component

---
+++ src/components/army-builder.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Points from './points.svelte';
+	import Roster from './roster.svelte';
+	import Summary from './summary.svelte';
+
+	export let army: Army;
+</script>
+
+<div class="container mx-auto">
+	<Points />
+</div>
+
+<div class="container mx-auto grid grid-cols-2 gap-2">
+	<div><Roster {army} /></div>
+
+	<div><Summary enhancements={army.enhancements} /></div>
+</div>
--- src/routes/aeldari/+page.svelte
+++ src/routes/aeldari/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import { Alert } from 'flowbite-svelte';
-	import Points from '../../components/points.svelte';
-	import Roster from '../../components/roster.svelte';
-	import Summary from '../../components/summary.svelte';
 	import { aeldari } from '../../data/aeldari/army';
+	import ArmyBuilder from '../../components/army-builder.svelte';
 </script>
 
 <svelte:head>
@@ -18,12 +16,5 @@
 	</Alert>
 </div>
 
-<div class="container mx-auto">
-	<Points />
-</div>
-
-<div class="container mx-auto grid grid-cols-2 gap-2">
-	<div><Roster army={aeldari} /></div>
-
-	<div><Summary enhancements={aeldari.enhancements} /></div>
-</div>
+<ArmyBuilder army={aeldari} />
+```
