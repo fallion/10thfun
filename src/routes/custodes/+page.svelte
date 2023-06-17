db718180484753ff1fd8239e785c1bed4fada1e6
Simon Prochazka <simon@aevea.ee>
2023-06-18 00:30:38 +0200

    chore: consolidate reused code into ArmyBuilder component (#6)

--- src/routes/custodes/+page.svelte
+++ src/routes/custodes/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import { Alert } from 'flowbite-svelte';
-	import Points from '../../components/points.svelte';
-	import Roster from '../../components/roster.svelte';
-	import Summary from '../../components/summary.svelte';
 	import { custodes } from '../../data/custodes/army';
+	import ArmyBuilder from '../../components/army-builder.svelte';
 </script>
 
 <svelte:head>
@@ -14,12 +12,4 @@
 	<Alert color="red">This is currently very much a work in progress.</Alert>
 </div>
 
-<div class="container mx-auto">
-	<Points />
-</div>
-
-<div class="container mx-auto grid grid-cols-2 gap-2">
-	<div><Roster army={custodes} /></div>
-
-	<div><Summary enhancements={custodes.enhancements} /></div>
-</div>
+<ArmyBuilder army={custodes} />
