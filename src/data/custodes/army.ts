2d25617c6175f34f1ad6e4d923c6be0300ef06eb
Simon <simon@aevea.ee>
2023-06-17 20:27:08 +0200

    feat: add custodes

---
+++ src/data/custodes/army.ts
@@ -0,0 +1,8 @@
+import { enhancements } from './enhancements';
+import { units } from './units';
+
+export const custodes: Army = {
+	name: 'Custodes',
+	units: units,
+	enhancements: enhancements
+};
---
+++ src/data/custodes/units.ts
@@ -0,0 +1,176 @@
+export const units: ArmyUnit[] = [
+	{
+		name: 'Aleya',
+		costs: [[1, 80]],
+		keywords: ['Infantry', 'Character', 'Epic Hero', 'Imperium', 'Aleya']
+	},
+	{
+		name: 'Allarus Custodians',
+		costs: [
+			[2, 130],
+			[3, 195],
+			[5, 325],
+			[6, 390]
+		],
+		keywords: ['Infantry', 'Terminator', 'Imperium', 'Allarus Custodians', 'Adeptus Custodes']
+	},
+	{
+		name: 'Anathema Psykana Rhino',
+		costs: [[1, 75]],
+		keywords: [
+			'Vehicle',
+			'Transport',
+			'Dedicated Transport',
+			'Smoke',
+			'Imperium',
+			'Anathema Psykana',
+			'Rhino'
+		]
+	},
+	{
+		name: 'Blade Champion',
+		costs: [[1, 100]],
+		keywords: ['Infantry', 'Character', 'Imperium', 'Blade Champion', 'Adeptus Custodes']
+	},
+	{
+		name: 'Custodian Guard',
+		costs: [
+			[4, 180],
+			[5, 225],
+			[9, 405],
+			[10, 450]
+		],
+		keywords: ['Infantry', 'Battleline', 'Imperium', 'Custodian Guard', 'Adeptus Custodes']
+	},
+	{
+		name: 'Custodian Wardens',
+		costs: [
+			[3, 150],
+			[6, 300]
+		],
+		keywords: ['Infantry', 'Imperium', 'Custodian Wardens', 'Adeptus Custodes']
+	},
+	{
+		name: 'Knight Centura',
+		costs: [[1, 75]],
+		keywords: ['Infantry', 'Character', 'Imperium', 'Anathema Psykana', 'Knight-Centura']
+	},
+	{
+		name: 'Prosecutors',
+		costs: [
+			[4, 40],
+			[5, 50],
+			[9, 90],
+			[10, 100]
+		],
+		keywords: ['Infantry', 'Battleline', 'Imperium', 'Anathema Psykana', 'Prosecutors']
+	},
+	{
+		name: 'Shield-Captain',
+		costs: [[1, 120]],
+		keywords: ['Infantry', 'Character', 'Imperium', 'Shield-Captain', 'Adeptus Custodes']
+	},
+	{
+		name: 'Shield-Captain in Allarus Terminator Armour',
+		costs: [[1, 120]],
+		keywords: [
+			'Infantry',
+			'Character',
+			'Imperium',
+			'Terminator',
+			'Shield-Captain',
+			'Adeptus Custodes'
+		]
+	},
+	{
+		name: 'Shield-Captain in Dawneagle Jetbike',
+		costs: [[1, 145]],
+		keywords: [
+			'Mounted',
+			'Character',
+			'Fly',
+			'Imperium',
+			'Dawneagle Jetbike',
+			'Shield-Captain',
+			'Adeptus Custodes'
+		]
+	},
+	{
+		name: 'Trajann Valoris',
+		costs: [[1, 145]],
+		keywords: [
+			'Infantry',
+			'Character',
+			'Epic Hero',
+			'Imperium',
+			'Trajann Valoris',
+			'Adeptus Custodes'
+		]
+	},
+	{
+		name: 'Valerian',
+		costs: [[1, 115]],
+		keywords: [
+			'Infantry',
+			'Character',
+			'Epic Hero',
+			'Imperium',
+			' Shield-Captain',
+			'Valerian',
+			'Adeptus Custodes'
+		]
+	},
+	{
+		name: 'Venerable Contemptor Dreadnought',
+		costs: [[1, 185]],
+		keywords: [
+			'Vehicle',
+			'Walker',
+			'Imperium',
+			' Venerable Contemptor Dreadnought',
+			'Adeptus Custodes'
+		]
+	},
+	{
+		name: 'Venerable Land Raider',
+		costs: [[1, 260]],
+		keywords: [
+			'Vehicle',
+			'Imperium',
+			'Transport',
+			'Smoke',
+			'Venerable Land Raider',
+			'Adeptus Custodes'
+		]
+	},
+	{
+		name: 'Vertus Praetors',
+		costs: [
+			[2, 180],
+			[3, 270],
+			[5, 450],
+			[6, 540]
+		],
+		keywords: ['Mounted', 'Fly', 'Imperium', 'Vertus Praetors', 'Adeptus Custodes']
+	},
+	{
+		name: 'Vigilators',
+		costs: [
+			[4, 60],
+			[5, 75],
+			[9, 135],
+			[10, 150]
+		],
+		keywords: ['Infantry', 'Imperium', 'Anathema Psykana', 'Vigilators']
+	},
+	{
+		name: 'Witchseekers',
+		costs: [
+			[4, 60],
+			[5, 75],
+			[9, 135],
+			[10, 150]
+		],
+		keywords: ['Infantry', 'Imperium', 'Anathema Psykana', 'Witchseekers']
+	}
+];
