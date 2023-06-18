1ad400fcf30c849c9b602a8ba693f57385e029b8
Simon Prochazka <simon@aevea.ee>
2023-06-18 23:03:51 +0200

    feat: add Grey Knights (#9)

---
+++ src/data/grey-knights/units.ts
@@ -0,0 +1,208 @@
+export const units: ArmyUnit[] = [
+	{
+		name: 'Brother-Captain',
+		costs: [[1, 95]],
+		keywords: ['Infantry', 'Character', 'Psyker', 'Terminator', 'Imperium', 'Brother-Captain']
+	},
+	{
+		name: 'Brother-Captain Stern',
+		costs: [[1, 110]],
+		keywords: [
+			'Infantry',
+			'Character',
+			'Epic Hero',
+			'Psyker',
+			'Imperium',
+			'Terminator',
+			'Brother-Captain Stern'
+		]
+	},
+	{
+		name: 'Brotherhood Champion',
+		costs: [[1, 85]],
+		keywords: ['Infantry', 'Character', 'Psyker', 'Imperium', 'Brotherhood Champion']
+	},
+	{
+		name: 'Brotherhood Chaplain',
+		costs: [[1, 90]],
+		keywords: ['Infantry', 'Character', 'Psyker', 'Terminator', 'Imperium', 'Brotherhood Chaplain']
+	},
+	{
+		name: 'Brotherhood Librarian',
+		costs: [[1, 110]],
+		keywords: ['Infantry', 'Character', 'Terminator', 'Psyker', 'Imperium', 'Brotherhood Librarian']
+	},
+	{
+		name: 'Brotherhood Techmarine',
+		costs: [[1, 85]],
+		keywords: ['Infantry', 'Character', 'Psyker', 'Imperium', 'Brotherhood Techmarine']
+	},
+	{
+		name: 'Brotherhood Terminator Squad',
+		costs: [
+			[5, 225],
+			[10, 450]
+		],
+		keywords: [
+			'Infantry',
+			'Battleline',
+			'Psyker',
+			'Terminator',
+			'Grenades',
+			'Imperium',
+			'Brotherhood Terminator Squad'
+		]
+	},
+	{
+		name: 'Castellan Crowe',
+		costs: [[1, 95]],
+		keywords: ['Infantry', 'Character', 'Epic Hero', 'Psyker', 'Imperium', 'Castellan Crowe']
+	},
+	{
+		name: 'Grand Master',
+		costs: [[1, 115]],
+		keywords: [
+			'Infantry',
+			'Character',
+			'Psyker',
+			'Grenades',
+			'Terminator',
+			'Imperium',
+			'Grand Master'
+		]
+	},
+	{
+		name: 'Grand Master in Nemesis Dreadknight',
+		costs: [[1, 245]],
+		keywords: [
+			'Vehicle',
+			'Character',
+			'Walker',
+			'Psyker',
+			'Imperium',
+			'Grand Master in Nemesis Dreadknight'
+		]
+	},
+	{
+		name: 'Grand Master Voldus',
+		costs: [[1, 115]],
+		keywords: [
+			'Infantry',
+			'Character',
+			'Epic Hero',
+			'Psyker',
+			'Terminator',
+			'Imperium',
+			'Grand Master Voldus'
+		]
+	},
+	{
+		name: 'Grey Knights Land Raider',
+		costs: [[1, 270]],
+		keywords: ['Vehicle', 'Transport', 'Smoke', 'Imperium', 'Land Raider']
+	},
+	{
+		name: 'Grey Knights Land Raider Crusader',
+		costs: [[1, 250]],
+		keywords: ['Vehicle', 'Transport', 'Smoke', 'Grenades', 'Imperium', 'Land Raider', 'Crusader']
+	},
+	{
+		name: 'Grey Knights Land Raider Redeemer',
+		costs: [[1, 290]],
+		keywords: ['Vehicle', 'Transport', 'Smoke', 'Grenades', 'Imperium', 'Land Raider', 'Redeemer']
+	},
+	{
+		name: 'Grey Knights Razorback',
+		costs: [[1, 95]],
+		keywords: ['Vehicle', 'Transport', 'Dedicated Transport', 'Smoke', 'Imperium', 'Razorback']
+	},
+	{
+		name: 'Grey Knights Rhino',
+		costs: [[1, 80]],
+		keywords: ['Vehicle', 'Transport', 'Dedicated Transport', 'Smoke', 'Imperium', 'Rhino']
+	},
+	{
+		name: 'Grey Knights Stormhawk Interceptor',
+		costs: [[1, 160]],
+		keywords: ['Vehicle', 'Aircraft', 'Fly', 'Smoke', 'Imperium', 'Stormhawk Interceptor']
+	},
+	{
+		name: 'Grey Knights Stormraven Gunship',
+		costs: [[1, 265]],
+		keywords: ['Vehicle', 'Aircraft', 'Transport', 'Fly', 'Imperium', 'Stormraven Gunship']
+	},
+	{
+		name: 'Grey Knights Stormtalon Gunship',
+		costs: [[1, 170]],
+		keywords: ['Vehicle', 'Aircraft', 'Fly', 'Imperium', 'Stormtalon Gunship']
+	},
+	{
+		name: 'Grey Knights Venerable Dreadnought',
+		costs: [[1, 155]],
+		keywords: ['Vehicle', 'Walker', 'Psyker', 'Smoke', 'Imperium', 'Venerable Dreadnought']
+	},
+	{
+		name: 'Interceptor Squad',
+		costs: [
+			[5, 160],
+			[10, 320]
+		],
+		keywords: ['Infantry', 'Psyker', 'Fly', 'Grenades', 'Imperium', 'Interceptor Squad']
+	},
+	{
+		name: 'Kaldor Draigo',
+		costs: [[1, 155]],
+		keywords: [
+			'Infantry',
+			'Character',
+			'Epic Hero',
+			'Psyker',
+			'Terminator',
+			'Grenades',
+			'Imperium',
+			'Kaldor Draigo'
+		]
+	},
+	{
+		name: 'Nemesis Dreadknight',
+		costs: [[1, 215]],
+		keywords: ['Vehicle', 'Walker', 'Psyker', 'Imperium', 'Nemesis Dreadknight']
+	},
+	{
+		name: 'Paladin Squad',
+		costs: [
+			[5, 255],
+			[10, 510]
+		],
+		keywords: ['Infantry', 'Psyker', 'Terminator', 'Grenades', 'Imperium', 'Paladin Squad']
+	},
+	{
+		name: 'Purgation Squad',
+		costs: [
+			[5, 165],
+			[10, 275]
+		],
+		keywords: ['Infantry', 'Psyker', 'Grenades', 'Imperium', 'Purgation Squad']
+	},
+	{
+		name: 'Puriﬁer Squad',
+		costs: [
+			[5, 165],
+			[10, 275]
+		],
+		keywords: ['Infantry', 'Psyker', 'Grenades', 'Imperium', 'Purifier Squad']
+	},
+	{
+		name: 'Servitors',
+		costs: [[4, 50]],
+		keywords: ['Infantry', 'Imperium', 'Servitors']
+	},
+	{
+		name: 'Strike Squad',
+		costs: [
+			[5, 135],
+			[10, 270]
+		],
+		keywords: ['Infantry', 'Battleline', 'Psyker', 'Grenades', 'Imperium', 'Strike Squad']
+	}
+];
