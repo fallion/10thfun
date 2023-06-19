a1f99e913003c1df1fcccca388744852c22a99ee
Simon Prochazka <simon@aevea.ee>
2023-06-19 15:10:20 +0200

    feat: add detail view (#14)

--- src/components/army-builder.svelte
+++ src/components/army-builder.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
+	import { TabItem, Tabs } from 'flowbite-svelte';
 	import Points from './points.svelte';
 	import Roster from './roster.svelte';
 	import Summary from './summary.svelte';
 	import Sync from './sync.svelte';
+	import Details from './details.svelte';
 
 	export let army: Army;
+	let visibleTab = 'builder';
+	let tabs = ['builder', 'details'];
+
+	function changeTab(tab: string) {
+		if (!tabs.includes(tab)) {
+			return;
+		}
+		visibleTab = tab;
+	}
 </script>
 
 <Sync armyName={army.name} />
@@ -13,8 +24,15 @@
 	<Points />
 </div>
 
-<div class="container mx-auto grid grid-cols-2 gap-2">
-	<div><Roster {army} /></div>
+<Tabs>
+	<TabItem open={visibleTab === 'builder'} title="Builder">
+		<div class="container mx-auto grid grid-cols-2 gap-2">
+			<div><Roster {army} /></div>
 
-	<div><Summary enhancements={army.enhancements} /></div>
-</div>
+			<div><Summary enhancements={army.enhancements} /></div>
+		</div>
+	</TabItem>
+	<TabItem open={visibleTab === 'details'} title="Details">
+		<Details />
+	</TabItem>
+</Tabs>
