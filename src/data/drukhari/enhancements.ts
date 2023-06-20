f61a17313033e50ff031c88d7b884d8966867fe7
Simon <simon@aevea.ee>
2023-06-20 10:59:25 +0200

    feat: add brokhari

---
+++ src/data/drukhari/enhancements.ts
@@ -0,0 +1,22 @@
+export const enhancements: Enhancement[] = [
+	{
+		name: 'Crucible of Malediction',
+		cost: 10,
+		requiredKeywords: ['Character', 'Haemonculus']
+	},
+	{
+		name: 'Blood Dancer',
+		cost: 20,
+		requiredKeywords: ['Character', 'Succubus']
+	},
+	{
+		name: 'Labyrinthine Cunning',
+		cost: 30,
+		requiredKeywords: ['Character', 'Archon']
+	},
+	{
+		name: 'The Art of Pain',
+		cost: 25,
+		requiredKeywords: ['Character']
+	}
+];
---
+++ src/data/drukhari/units.ts
@@ -0,0 +1,165 @@
+export const units: ArmyUnit[] = [
+	{
+		name: 'Archon',
+		costs: [[1, 85]],
+		keywords: ['Infantry', 'Kabal', 'Character', 'Aeldari', 'Archon']
+	},
+	{
+		name: 'Beastmaster',
+		costs: [[7, 120]],
+		keywords: ['Beasts', 'Aeldari', 'Fly', 'Mounted', 'Character', 'Fly', 'Beastmaster']
+	},
+	{
+		name: 'Court of the Archon',
+		costs: [[4, 95]],
+		keywords: ['Infantry', 'Kabal', 'Aeldari', 'Court of the Archon']
+	},
+	{
+		name: 'Cronos',
+		costs: [
+			[1, 50],
+			[2, 100]
+		],
+		keywords: ['Monster', 'Haemonculus Covens', 'Fly', 'Aeldari', 'Cronos']
+	},
+	{
+		name: 'Drazhar',
+		costs: [[1, 105]],
+		keywords: ['Infantry', 'Character', 'Epic Hero', 'Aeldari', 'Drazhar']
+	},
+	{
+		name: 'Grotesques',
+		costs: [
+			[3, 105],
+			[6, 210]
+		],
+		keywords: ['Infantry', 'Haemonculus Covens', 'Aeldari', 'Grotesques']
+	},
+	{
+		name: 'Haemonculus',
+		costs: [[1, 75]],
+		keywords: ['Infantry', 'Haemonculus Covens', 'Character', 'Aeldari', 'Haemonculus']
+	},
+	{
+		name: 'Hellions',
+		costs: [
+			[5, 110],
+			[10, 220]
+		],
+		keywords: ['Mounted', 'Fly', 'Aeldari', 'Wych Cult', 'Hellions']
+	},
+	{
+		name: 'Incubi',
+		costs: [
+			[5, 85],
+			[10, 170]
+		],
+		keywords: ['Infantry', 'Aeldari', 'Incubi']
+	},
+	{
+		name: 'Kabalite Warriors',
+		costs: [[10, 120]],
+		keywords: ['Infantry', 'Kabal', 'Battleline', 'Aeldari', 'Kabalite Warriors']
+	},
+	{
+		name: 'Lelith Hesperax',
+		costs: [[1, 105]],
+		keywords: [
+			'Infantry',
+			'Wych Cult',
+			'Character',
+			'Epic Hero',
+			'Aeldari',
+			'Succubus',
+			'Lelith Hesperax'
+		]
+	},
+	{
+		name: 'Mandrakes',
+		costs: [
+			[5, 70],
+			[10, 140]
+		],
+		keywords: ['Infantry', 'Aeldari', 'Mandrakes']
+	},
+	{
+		name: 'Raider',
+		costs: [[1, 90]],
+		keywords: ['Vehicle', 'Transport', 'Dedicated Transport', 'Fly', 'Aeldari', 'Raider']
+	},
+	{
+		name: 'Ravager',
+		costs: [[1, 95]],
+		keywords: ['Vehicle', 'Kabal', 'Fly', 'Aeldari', 'Ravager']
+	},
+	{
+		name: 'Razorwing Jetfighter',
+		costs: [[1, 170]],
+		keywords: ['Vehicle', 'Fly', 'Aircraft', 'Aeldari', 'Razorwing Jetfighter']
+	},
+	{
+		name: 'Reavers',
+		costs: [
+			[3, 75],
+			[6, 150]
+		],
+		keywords: ['Mounted', 'Wych Cult', 'Fly', 'Aeldari', 'Reavers']
+	},
+	{
+		name: 'Scourges',
+		costs: [
+			[5, 120],
+			[10, 200]
+		],
+		keywords: ['Infantry', 'Fly', 'Grenades', 'Jump Pack', 'Aeldari', 'Scourges']
+	},
+	{
+		name: 'Succubus',
+		costs: [[1, 70]],
+		keywords: ['Infantry', 'Wych Cult', 'Character', 'Aeldari', 'Succubus']
+	},
+	{
+		name: 'Talos',
+		costs: [
+			[1, 90],
+			[2, 180]
+		],
+		keywords: ['Monster', 'Haemonculus Covens', 'Fly', 'Aeldari', 'Talos']
+	},
+	{
+		name: 'Urien Rakarth',
+		costs: [[1, 105]],
+		keywords: [
+			'Infantry',
+			'Haemonculus Covens',
+			'Character',
+			'Epic Hero',
+			'Aeldari',
+			'Haemonculus',
+			'Urien Rakarth'
+		]
+	},
+	{
+		name: 'Venom',
+		costs: [[1, 80]],
+		keywords: ['Vehicle', 'Transport', 'Dedicated transport', 'Fly', 'Aeldari', 'Venom']
+	},
+	{
+		name: 'Voidraven Bomber',
+		costs: [[1, 195]],
+		keywords: ['Vehicle', 'Aircraft', 'Aeldari', 'Voidraven Bomber']
+	},
+	{
+		name: 'Wracks',
+		costs: [
+			[5, 65],
+			[10, 130]
+		],
+		keywords: ['Infantry', 'Battleline', 'Haemonculus Covens', 'Aeldari', 'Wracks']
+	},
+	{
+		name: 'Wyches',
+		costs: [[10, 110]],
+		keywords: ['Infantry', 'Battleline', 'Wych Cult', 'Aeldari', 'Wyches']
+	}
+];
