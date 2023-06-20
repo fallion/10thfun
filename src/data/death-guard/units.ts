e7e53f747dabbe13d19c5a8b320eaeca9a6d75e6
Simon Prochazka <simon@aevea.ee>
2023-06-20 15:19:28 +0200

    feat: add deathguard (#18)

---
+++ src/data/death-guard/units.ts
@@ -0,0 +1,220 @@
+export const units: ArmyUnit[] = [
+	{
+		name: 'Biologus Putrifier',
+		costs: [[1, 60]],
+		keywords: ['Infantry', 'Character', 'Chaos', 'Nurgle', 'Grenades', 'Biologus Putrifier']
+	},
+	{
+		name: 'Blightlord Terminators',
+		costs: [
+			[5, 175],
+			[10, 350]
+		],
+		keywords: ['Infantry', 'Chaos', 'Nurgle', 'Terminator', 'Blightlord Terminators']
+	},
+	{
+		name: 'Death Guard Chaos Lord',
+		costs: [[1, 75]],
+		keywords: ['Infantry', 'Character', 'Chaos', 'Nurgle', 'Grenades', 'Chaos Lord']
+	},
+	{
+		name: 'Death Guard Chaos Lord in Terminator Armour',
+		costs: [[1, 100]],
+		keywords: ['Infantry', 'Character', 'Chaos', 'Nurgle', 'Terminator', 'Chaos Lord']
+	},
+	{
+		name: 'Death Guard Chaos Spawn',
+		costs: [[2, 75]],
+		keywords: ['Beast', 'Chaos', 'Nurgle', 'Spawn']
+	},
+	{
+		name: 'Death Guard Cultists',
+		costs: [
+			[10, 55],
+			[20, 110]
+		],
+		keywords: ['Infantry', 'Chaos', 'Nurgle', 'Cultists']
+	},
+	{
+		name: 'Death Guard Daemon Prince',
+		costs: [[1, 170]],
+		keywords: ['Monster', 'Character', 'Chaos', 'Nurgle', 'Psyker', 'Daemon', 'Daemon Prince']
+	},
+	{
+		name: 'Death Guard Daemon Prince with Wings',
+		costs: [[1, 215]],
+		keywords: [
+			'Monster',
+			'Character',
+			'Fly',
+			'Chaos',
+			'Nurgle',
+			'Psyker',
+			'Daemon',
+			'Daemon Prince with Wings'
+		]
+	},
+	{
+		name: 'Death Guard Defiler',
+		costs: [[1, 205]],
+		keywords: ['Vehicle', 'Walker', 'Chaos', 'Nurgle', 'Daemon', 'Smoke', 'Defiler']
+	},
+	{
+		name: 'Death Guard Helbrute',
+		costs: [[1, 155]],
+		keywords: ['Vehicle', 'Walker', 'Chaos', 'Nurgle', 'Helbrute']
+	},
+	{
+		name: 'Death Guard Icon Bearer',
+		costs: [[1, 55]],
+		keywords: ['Infantry', 'Character', 'Chaos', 'Nurgle', 'Grenades', 'Death Guard Icon Bearer']
+	},
+	{
+		name: 'Death Guard Land Raider',
+		costs: [[1, 250]],
+		keywords: ['Vehicle', 'Chaos', 'Nurgle', 'Transport', 'Smoke', 'Land Raider']
+	},
+	{
+		name: 'Death Guard Predator Annihilator',
+		costs: [[1, 130]],
+		keywords: ['Vehicle', 'Chaos', 'Nurgle', 'Smoke', 'Predator Annihilator']
+	},
+	{
+		name: 'Death Guard Predator Destructor',
+		costs: [[1, 140]],
+		keywords: ['Vehicle', 'Chaos', 'Nurgle', 'Smoke', 'Predator Destructor']
+	},
+	{
+		name: 'Death Guard Rhino',
+		costs: [[1, 80]],
+		keywords: ['Vehicle', 'Chaos', 'Nurgle', 'Transport', 'Dedicated Transport', 'Smoke', 'Rhino']
+	},
+	{
+		name: 'Death Guard Sorcerer in Terminator Armour',
+		costs: [[1, 80]],
+		keywords: ['Infantry', 'Character', 'Psyker', 'Chaos', 'Nurgle', 'Terminator', 'Sorcerer']
+	},
+	{
+		name: 'Deathshroud Terminators',
+		costs: [
+			[3, 140],
+			[6, 280]
+		],
+		keywords: ['Infantry', 'Chaos', 'Nurgle', 'Terminator', 'Deathshroud Terminators']
+	},
+	{
+		name: 'Foetid Bloat-drone',
+		costs: [[1, 135]],
+		keywords: ['Vehicle', 'Fly', 'Chaos', 'Nurgle', 'Daemon', 'Foetid Bloat-drone']
+	},
+	{
+		name: 'Foul Blightspawn',
+		costs: [[1, 55]],
+		keywords: ['Infantry', 'Character', 'Chaos', 'Nurgle', 'Grenades', 'Foul Blightspawn']
+	},
+	{
+		name: 'Lord of Contaigon',
+		costs: [[1, 100]],
+		keywords: [
+			'Infantry',
+			'Character',
+			'Chaos',
+			'Nurgle',
+			'Terminator',
+			'Grenades',
+			'Lord of Contagion'
+		]
+	},
+	{
+		name: 'Lord of Virulence',
+		costs: [[1, 115]],
+		keywords: ['Infantry', 'Character', 'Chaos', 'Nurgle', 'Terminator', 'Lord of Virulence']
+	},
+	{
+		name: 'Malignant Plaguecaster',
+		costs: [[1, 75]],
+
+		keywords: ['Infantry', 'Character', 'Chaos', 'Nurgle', 'Psyker', 'Malignant Plaguecaster']
+	},
+	{
+		name: 'Miasmic Malignifier',
+		costs: [[1, 115]],
+		keywords: ['Fortification', 'Chaos', 'Nurgle', 'Miasmic Malignifier']
+	},
+	{
+		name: 'Mortarion',
+		costs: [[1, 370]],
+		keywords: [
+			'Monster',
+			'Psyker',
+			'Fly',
+			'Character',
+			'Epic Hero',
+			'Daemon',
+			'Grenades',
+			'Chaos',
+			'Nurgle',
+			'Primarch',
+			'Mortarion'
+		]
+	},
+	{
+		name: 'Myphitic Blight-haulers',
+		costs: [
+			[1, 115],
+			[2, 230],
+			[3, 345]
+		],
+		keywords: ['Vehicle', 'Chaos', 'Nurgle', 'Daemon', 'Smoke', 'Myphitic Blight-haulers']
+	},
+	{
+		name: 'Noxious Blightbringer',
+		costs: [[1, 60]],
+		keywords: ['Infantry', 'Character', 'Chaos', 'Nurgle', 'Noxious Blightbringer']
+	},
+	{
+		name: 'Plague Marines',
+		costs: [
+			[5, 100],
+			[10, 200]
+		],
+		keywords: ['Infantry', 'Chaos', 'Nurgle', 'Grenades', 'Battleline', 'Plague Marines']
+	},
+	{
+		name: 'Plague Surgeon',
+		costs: [[1, 65]],
+		keywords: ['Infantry', 'Character', 'Chaos', 'Nurgle', 'Plague Surgeon']
+	},
+	{
+		name: 'Plagueburst Crawler',
+		costs: [[1, 175]],
+		keywords: ['Vehicle', 'Chaos', 'Nurgle', 'Daemon', 'Plagueburst Crawler']
+	},
+	{
+		name: 'Poxwalkers',
+		costs: [
+			[10, 60],
+			[20, 120]
+		],
+		keywords: ['Infantry', 'Chaos', 'Nurgle', 'Poxwalkers']
+	},
+	{
+		name: 'Tallyman',
+		costs: [[1, 55]],
+		keywords: ['Infantry', 'Character', 'Chaos', 'Nurgle', 'Tallyman']
+	},
+	{
+		name: 'Typhus',
+		costs: [[1, 115]],
+		keywords: [
+			'Infantry',
+			'Character',
+			'Epic Hero',
+			'Chaos',
+			'Nurgle',
+			'Psyker',
+			'Terminator',
+			'Typhus'
+		]
+	}
+];
