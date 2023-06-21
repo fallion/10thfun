ac6414063ea6177368fd573d00e50907cd15398c
Simon Prochazka <simon@aevea.ee>
2023-06-21 11:40:45 +0200

    feat: add genestealers (#21)

---
+++ src/data/generstealer-cults/enhancements.ts
@@ -0,0 +1,22 @@
+export const enhancements: Enhancement[] = [
+	{
+		name: 'Focus of Adoration',
+		cost: 10,
+		requiredKeywords: ['Character']
+	},
+	{
+		name: 'Inscrutable Cunning',
+		cost: 30,
+		requiredKeywords: ['Character']
+	},
+	{
+		name: 'Meticulous Planner',
+		cost: 40,
+		requiredKeywords: ['Character']
+	},
+	{
+		name: 'Prowling Agitant',
+		cost: 15,
+		requiredKeywords: ['Character']
+	}
+];
