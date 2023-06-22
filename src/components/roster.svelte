ab6b4c538c4c297b9fd5562c92a0ff0beb1c5cc3
Simon Prochazka <simon@aevea.ee>
2023-06-22 17:20:44 +0200

    feat: add search for units (#28)

--- src/components/roster.svelte
+++ src/components/roster.svelte
@@ -6,28 +6,35 @@
 		TableBodyRow,
 		TableHead,
 		TableHeadCell,
-		Button
+		Button,
+		TableSearch
 	} from 'flowbite-svelte';
 	import { addUnit } from '../data/store';
 
 	export let army: Army;
+	let searchTerm = '';
+	$: filteredItems = army.units.filter(
+		(item) => item.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
+	);
 </script>
 
-<Table striped={true} hoverable={true} shadow>
-	<TableHead>
-		<TableHeadCell>Name</TableHeadCell>
-		<TableHeadCell>Base cost</TableHeadCell>
-		<TableHeadCell>Add</TableHeadCell>
-	</TableHead>
-	<TableBody>
-		{#each army.units as unit}
-			<TableBodyRow>
-				<TableBodyCell>{unit.name}</TableBodyCell>
-				<TableBodyCell>{unit.costs[0][1]} ({unit.costs[0][0]} models)</TableBodyCell>
-				<TableBodyCell>
-					<Button color="blue" on:click={() => addUnit(unit)}>+</Button>
-				</TableBodyCell>
-			</TableBodyRow>
-		{/each}
-	</TableBody>
-</Table>
+<TableSearch placeholder="Search by unit name" hoverable={true} bind:inputValue={searchTerm}>
+	<Table striped={true} hoverable={true} shadow>
+		<TableHead>
+			<TableHeadCell>Name</TableHeadCell>
+			<TableHeadCell>Base cost</TableHeadCell>
+			<TableHeadCell>Add</TableHeadCell>
+		</TableHead>
+		<TableBody>
+			{#each filteredItems as unit}
+				<TableBodyRow>
+					<TableBodyCell>{unit.name}</TableBodyCell>
+					<TableBodyCell>{unit.costs[0][1]} ({unit.costs[0][0]} models)</TableBodyCell>
+					<TableBodyCell>
+						<Button color="blue" on:click={() => addUnit(unit)}>+</Button>
+					</TableBodyCell>
+				</TableBodyRow>
+			{/each}
+		</TableBody>
+	</Table>
+</TableSearch>
