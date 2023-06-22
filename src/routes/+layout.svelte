c0a30776a49182fda162826cf6c902bbb705098b
Simon <simon@aevea.ee>
2023-06-22 17:03:18 +0200

    feat: add loader

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { navigating } from '$app/stores';
+	import { Spinner } from 'flowbite-svelte';
 	import '../app.postcss';
 	import Banner from '../components/banner.svelte';
 	import Navbar from '../components/navbar.svelte';
@@ -8,5 +10,11 @@
 
 <Banner />
 <div class="container mx-auto">
-	<slot />
+	{#if $navigating}
+		<div class="text-center">
+			<Spinner />
+		</div>
+	{:else}
+		<slot />
+	{/if}
 </div>
